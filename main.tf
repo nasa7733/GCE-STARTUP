6a953b7cbd88504081ec018a392ecbd3ef9b3236
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2020-03-25 12:26:08 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  credentials = file("terraform.json")
+  project = "	lyrical-shore-262101"
+  region  = "us-central1"
+  
+}
+
+resource "google_compute_instance" "vm-instance" {
+  name         = "target-new-vm"
+  machine_type = "f1-micro"
+  zone         = "us-central1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+ 
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
