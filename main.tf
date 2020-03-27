756e7d0455012ef4414207031cd2cdf0cf04b204
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2020-03-27 10:21:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 provider "google" {
   #credentials = file("terraform.json")
-  project = "	lyrical-shore-262101"
-  region  = "us-central1"
+  project = "lyrical-shore-262101"
+  region  = "us-east1"
   
 }
 
 resource "google_compute_instance" "vm-instance" {
   name         = "target-new-vm"
   machine_type = "f1-micro"
-  zone         = "us-central1-c"
+  zone         = "us-east1-b"
 
   boot_disk {
     initialize_params {
